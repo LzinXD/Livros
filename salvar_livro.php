3697146bf772eb2d27e26c4f5c6b4a92d65b2a45
LzinXD <sukimomora@gmail.com>
2025-07-21 20:04:19 -0300

    Update salvar_livro.php
    2

--- salvar_livro.php
+++ salvar_livro.php
@@ -2,15 +2,15 @@
 require 'conexao.php';
 require 'livros.php';
 
-$livros= new Livros($pdo);
-$livros->setNome($_POST['nome']);
-$livros->setAutor($_POST['autor']);
-$livros->setGenero($_POST['genero']);
-$livros->setEditora($_POST['editora']);
-$livros->setAnoLancamento($_POST['ano_lancamento']);
+$livro = new Livros($pdo);
+$livro->setNome($_POST['nome']);
+$livro->setAutor($_POST['autor']);
+$livro->setGenero($_POST['genero']);
+$livro->setEditora($_POST['editora']);
+$livro->setAnoLancamento($_POST['ano_lancamento']);
 
-if ($livros->salvar()) {
-    echo "livro cadastrado com sucesso! <a href='listar_livro.php'>Ver lista</a>";
+if ($livro->salvar()) {
+    echo "Livro cadastrado com sucesso! <a href='listar_livro.php'>Ver lista</a>";
 } else {
     echo "Erro ao cadastrar.";
 }
