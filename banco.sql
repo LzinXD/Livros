ed9ba3f73acc5acb75f411c0f20d78477734a953
LzinXD <sukimomora@gmail.com>
2025-07-21 19:59:49 -0300

    Update banco.sql
    2 mudança

--- banco.sql
+++ banco.sql
@@ -1,12 +1,11 @@
 create database cadastro_livros;
 use cadastro_livros;
-
--- Tabela para armazenar informações dos livros
+ 
 create table livros(
 id INT auto_increment primary key,
 nome varchar(100) not null,
 autor varchar (100) not null,
 genero varchar(50) not null,
 editora varchar(50) not null,
-ano_lancamento YEAR not null
+ano_lancamento int(4) not null
 );
